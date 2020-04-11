dfed1d1fb4ed357db7436d8a9cbe86d33d32b1cb
Igor Bechthold <igor.bechthold.de@gmail.com>
2020-04-11 21:02:02 +0200

    g

--- week_05/data.py
+++ week_05/data.py
@@ -2,3 +2,8 @@ from ex_05_client_for_metrics import Client
 from ex_05_client_for_metrics import ClientError
 client = Client("127.0.0.1", 8888, timeout=15)
 client.get("*")
+
+import socket
+sock = socket.create_connection(('127.0.0.1', 8888), 15)
+sock.send(b'data from client2')
+sock.recv(1024)
